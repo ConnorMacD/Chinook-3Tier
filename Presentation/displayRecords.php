dca87eafcceb0e0610ea595cfb9af024e5d0963f
Connor MacDonald <cmacd.94@gmail.com>
2014-12-03 18:38:49 -0400

    create displayRecords display

---
+++ Presentation/displayRecords.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Connor
+ * Date: 12/3/2014
+ * Time: 6:25 PM
+ */
+
+?>
+<html>
+
+    <head>
+        <title>Chinook Database</title>
+        <link rel="stylesheet" href="css/app.css"/>
+    </head>
+
+    <body>
+        <h1>Current Tracks:</h1>
+        <table border="1">
+            <thead>
+            <tr>
+                <th>Track ID</th>
+                <th>Name</th>
+                <th>Album</th>
+                <th>Artist</th>
+                <th>Media Type</th>
+                <th>Genre</th>
+                <th>Composer(s)</th>
+                <th>Milliseconds</th>
+                <th>Bytes</th>
+                <th>Unit Price</th>
+                <th>Add to Cart</th>
+            </tr>
+            </thead>
+
+            <tbody>
+
+            </tbody>
+        </table>
+    </body>
+</html>
