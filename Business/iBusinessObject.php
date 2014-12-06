69ade38253df517718cc9b8ec6dc994f6d20a7f7
Connor MacDonald <cmacd.94@gmail.com>
2014-12-06 11:46:21 -0400

    use for Track object

---
+++ Business/iBusinessObject.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Connor
+ * Date: 12/3/2014
+ * Time: 7:50 PM
+ */
+
+interface iBusinessObject
+{
+    public static function retrieveSome($start,$count);
+    //public function save();
+}
