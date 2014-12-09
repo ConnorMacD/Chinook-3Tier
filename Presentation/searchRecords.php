0131b47c0e3bc3d73381cc80b45a8dcf8a9dfa9c
Connor MacDonald <cmacd.94@gmail.com>
2014-12-09 17:05:06 -0400

    commit searchRecords.php for ajax dataTable

---
+++ Presentation/searchRecords.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/app.css"/>
+        <link rel="stylesheet" href="css/jquery.dataTables.min.css"/>
+        <script src="js/jquery.js"></script>
+        <script src="js/jquery.dataTables.min.js"></script>
+        <script src="js/myGrid.js"></script>
+    </head>
+    <body>
+        <section>
+            <table id="grid">
+                <thead>
+                    <tr colspan="3">
+                        Available Music
+                    </tr>
+                    <tr>
+                        <th>Track ID</th>
+                        <th>Name</th>
+                        <th>Album</th>
+                        <th>Artist</th>
+                        <th>Media Type</th>
+                        <th>Genre</th>
+                        <th>Composer(s)</th>
+                        <th>Milliseconds</th>
+                        <th>Bytes</th>
+                        <th>Unit Price</th>
+                        <th>Add to Cart</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                <?php
+
+                require("../Business/Track.php");
+                $arrayOfTracks = Track::retrieveAll();
+
+                foreach($arrayOfTracks as $track):
+                    ?>
+                    <tr>
+                        <td><?php echo $track->getID(); ?></td>
+                        <td><?php echo $track->getName(); ?></td>
+                        <td><?php echo $track->getAlbum(); ?></td>
+                        <td><?php echo $track->getArtist(); ?></td>
+                        <td><?php echo $track->getMediaType(); ?></td>
+                        <td><?php echo $track->getGenre(); ?></td>
+                        <td><?php echo $track->getComposers(); ?></td>
+                        <td><?php echo $track->getMilliseconds(); ?></td>
+                        <td><?php echo $track->getBytes(); ?></td>
+                        <td><?php echo $track->getUnitPrice(); ?></td>
+                        <td><a href="cart.php?add=<?php echo $track->getID(); ?>">Add to Cart</a></td>
+                    </tr>
+                <?php endforeach;?>
+                </tbody>
+            </table>
+        </section>
+    </body>
+</html>
