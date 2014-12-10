2d78e3967b69386906df163cbf93c47ff68bbc51
Connor MacDonald <cmacd.94@gmail.com>
2014-12-09 20:18:07 -0400

    add click event to "add" class for ajax adding

--- Presentation/js/myGrid.js
+++ Presentation/js/myGrid.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
     $('#grid').dataTable();
+
+    $('#grid tbody').on("click", '.add', function(event) {
+        //event.preventDefault();
+        if ($(this).hasClass("enabled")) {
+            $(this).text("Added");
+            //$(this).removeAttr("href");
+            $(this).removeClass("enabled");
+            $(this).attr("disabled",true);
+            //$(this).off();
+            console.log("clicky clicky");
+        }
+    });
 });
